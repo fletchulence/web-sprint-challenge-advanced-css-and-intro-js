06c55376f5848a96ba9f5c857d791d8ce46134ee
David Fletcher <davidfletcher@Davids-MacBook-Air-2.local>
2021-08-13 13:03:30 -0700

    test

--- README.md
+++ README.md
@@ -24,6 +24,9 @@ Please answer the following questions below, you may edit the readme file to inc
 
 1. How would you describe acessibility on the web to someone new to programming?
 2. Talk about 3 different things you can do to ensure your website is accessible. 
+    - alt keys for images
+    - semantic HTML
+    - 
 3. How would you explain the concept of a variable to someone new to programming?
 4. What is the purpose of using functions in code?
 5. How do you access a key inside of an object inside of an array?
--- index.js
+++ index.js
@@ -208,17 +208,17 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+   console.log(artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
-
-
+   console.log(artists[2].bio);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
-There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
+There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = 'Vincent Van Gogh';
+console.log('task2', artists[8]);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -228,8 +228,8 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, x) {
+ return `the artist at index ${array[x].id} is ${array[x].name}`
 }
 
 
@@ -243,9 +243,19 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
+  let arraySplit = [];
+  let arrayNames = [];
+
+  for (let i = 0 ; i < array.length ; i++){
+     arraySplit = array[i].year.split(' - ')
+
+  
+    return arraySplit;
+  }
+  return arraySplit;
 }
+ console.log(get20s(artists))
 
 
 
@@ -258,8 +268,9 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, x) {
+  array.splice(x, 1);
+  return array.length;
 }
 
 
@@ -279,10 +290,22 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  const myArr =[
+    { 
+      'id': 27,
+      'name': 'David Fletcher', 
+      'years': '1994 - present',
+      'genre': 'Web Design', 
+      'nationality': 'American',
+      'bio': 'asldjff asdffl;jahsdf;akjsd aks sldf;asdlfjk ;alksjdklfal; skjd f;klajs . a;shdflja s;ldjfh ;ajshdf aas df.asdf;ljhas ;ldkfj ;lasd. asdl;jkfha s',
+      'paintings': '2000'
+    } 
+  ]
+  array.push(myArr);
+  return array; 
 }
-
+console.log('task6', addArtist(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -292,9 +315,19 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+  let arrayNames = [];
+
+  for (let i = 0 ; i < array.length ; i++){
+    if (array[i].paintings > 100){
+      arrayNames.push(array[i].name);
+    } /* else{
+      
+    } */
+  }
+  return arrayNames;
 }
+console.log('here@#$%^&*', lotsOfArt(artists));
 
 
 /* ***** END OF TASKS ***** */
